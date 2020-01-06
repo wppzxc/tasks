1bc134a2eac92c5980df3d91f6e3a35e8a83e7e9
wupengpeng <wupengpeng@kingsoft.com>
2020-01-06 17:12:42 +0800

    update qrcode

--- webApp/src/main.js
+++ webApp/src/main.js
@@ -5,12 +5,10 @@ import 'vant/lib/index.css'
 import App from './App.vue'
 import routes from './js/router/router'
 import store from './js/store/store'
-import VueClipboard from 'vue-clipboard2'
 import axios from 'axios'
 
 Vue.use(Vant);
 Vue.use(VueRouter);
-Vue.use(VueClipboard);
 Vue.prototype.$axios = axios;
 
 const router = new VueRouter({routes});
